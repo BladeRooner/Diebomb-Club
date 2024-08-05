660a147155304e36b4248e906666b8e5ea496bf2
Arano <Nick.Dreczkowski@gmail.com>
2024-08-05 12:57:47 +0200

    React

---
+++ diebomb-club/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Image from "G:/Images/00171679.jpg";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div>
+      <LazyLoadImage src={Image}
+        alt="Image Alt"
+      />
+     </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
